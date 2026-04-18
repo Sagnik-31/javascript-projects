e4d328c66d2f2618a6188f9049450b780ecbb6d6
Sagnik Ghosh <ghosh1234sagnik@gmail.com>
2026-04-18 19:28:35 +0530

    updated async await

--- async-await/script.js
+++ async-await/script.js
@@ -1,25 +1,30 @@
 const parent = document.getElementById("first")
 
 async function github(){
-    const response = await fetch("https://api.github.com/users")
-    const data = await response.json()
+    try{
+        const response = await fetch("https://api.github.com/users")
+        const data = await response.json()
 
-    for(let user of data){
-        const element = document.createElement('div') //each element is a card then appended to parent div
-        element.classList.add("user");
+        for(let user of data){
+            const element = document.createElement('div') //each element is a card then appended to parent div
+            element.classList.add("user");
 
-        const image = document.createElement('img');
-        image.src = user.avatar_url;
+            const image = document.createElement('img');
+            image.src = user.avatar_url;
 
-        const userName = document.createElement('h2');
-        userName.textContent = user.login;
+            const userName = document.createElement('h2');
+            userName.textContent = user.login;
 
-        const anchor = document.createElement('a');
-        anchor.href = user.html_url;
-        anchor.textContent = "visit profile";
+            const anchor = document.createElement('a');
+            anchor.href = user.html_url;
+            anchor.textContent = "visit profile";
 
-        element.append(image,userName,anchor);
-        parent.append(element);
+            element.append(image,userName,anchor);
+            parent.append(element);
+        }
+    }
+    catch(error){
+        console.log("Error fetching data:", error);
     }
 
 }
